d7860f3f77a6d276f0b3b752036cca1ac554ecc4
Oscar Renier <orenier@steamulo.com>
2022-09-06 16:35:54 +0200

    [REFACTO]: CSS injections removed

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -9,16 +9,16 @@ import './AppBar.css'
 const AppBar = () => {
     return (
         <Navbar className='app-navbar'>
-            <Navbar.Brand href="/" style={{ marginLeft: 25 }} >
+            <Navbar.Brand href="/">
                 <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png" alt="logo" style={{ maxHeight: "3rem" }} />
                 <h1> MyBouqins </h1>
             </Navbar.Brand>
-            <Nav style={{ marginLeft: "auto", marginRight: "3em" }}>
-                <Nav.Link className="hover-lined" href="/" style={{ fontSize: "1.25em", marginLeft: 15 }}>Accueil</Nav.Link>
-                <Nav.Link className="hover-lined" href="/categories" style={{ fontSize: "1.25em", marginLeft: 15 }}>Catégories</Nav.Link>
-                <Nav.Link className="hover-lined" href="/noslivres" style={{ fontSize: "1.25em", marginLeft: 15 }}>Nos Livres</Nav.Link>
-                <Nav.Link className="hover-lined" href="/horaires" style={{ fontSize: "1.25em", marginLeft: 15 }}>Horaires</Nav.Link>
-                <Nav.Link className="hover-lined" href="/reglement" style={{ fontSize: "1.25em", marginLeft: 15 }}>Règlement</Nav.Link>
+            <Nav className="nav">
+                <Nav.Link className="hover-lined" href="/">Accueil</Nav.Link>
+                <Nav.Link className="hover-lined" href="/categories">Catégories</Nav.Link>
+                <Nav.Link className="hover-lined" href="/nos-livres">Nos Livres</Nav.Link>
+                <Nav.Link className="hover-lined" href="/horaires">Horaires</Nav.Link>
+                <Nav.Link className="hover-lined" href="/reglement">Règlement</Nav.Link>
             </Nav>
         </Navbar>
     );
