ae5511364dba1b5320b6ece9382ece97ef3b82cb
Oscar Renier <orenier@steamulo.com>
2022-09-06 17:32:01 +0200

    [REFACTO]: Routes are now encapsulated in AppRouter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-
-//Pages
-import MainPage from "./pages";
-import CategoriesPage from './pages/categories';
-import OurbooksPage from './pages/ourbooks';
-import NotFoundPage from './pages/404';
-import HoursPage from './pages/hours';
-import RulesPage from './pages/rules';
-
+import AppRouter from "./routes/AppRouter";
 import './App.css';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/categories" element={<CategoriesPage />} />
-                <Route path="/nos-livres" element={<OurbooksPage />} />
-                <Route path="/horaires" element={<HoursPage />} />
-                <Route path="/reglement" element={<RulesPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </Router>
+        <AppRouter/>
     );
 }
 
