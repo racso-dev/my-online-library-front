43bb12ddf60af8dd0eaa27c03468e88e91c5de55
Oscar Renier <orenier@steamulo.com>
2022-09-07 11:47:36 +0200

    [ADD]: Authentication pages added

---
+++ src/pages/Connection.jsx
@@ -0,0 +1,39 @@
+import './Connection.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+
+import { Pages } from "../routes/AppRouter";
+import { signIn } from '../services/AuthService';
+
+const ConnexionPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const validate = () => (email.length > 0 && password.length > 0);
+    const handler = (event) => {
+        event.preventDefault();
+        signIn(email, password);
+        window.location.href = Pages.OUR_BOOKS;
+    };
+    return (
+        <div className="connexion">
+            <Form onSubmit={handler}>
+                <Form.Group className="form" controlId="email">
+                    <Form.Label className="form-label">Email</Form.Label>
+                    <Form.Control className="form-input" type="email" value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="form" controlId="password">
+                    <Form.Label className="form-label">Password</Form.Label>
+                    <Form.Control className="form-input" type="password" value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button className="button" type="submit" disabled={!validate()}> Connexion </Button>
+            </Form>
+            <a href={Pages.REGISTER}> Pas encore de compte ? Rendez-vous ici !</a>
+        </div>
+    );
+}
+
+export default ConnexionPage;
