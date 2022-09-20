d0a6e3f58aa0223f98d39ab54617ff6bcf4df621
Oscar Renier <orenier@steamulo.com>
2022-09-20 11:02:34 +0200

    [ADD]: Modal to visualize a book

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import BookModal from '../components/BookModal';
+import { Modal, Button } from 'react-bootstrap';
 import { getBooks } from '../services/BookService';
 import "./Categories.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CategoriesPage = ({ category }) => {
     const [books, setBooks] = useState([]);
+    const [show, setShow] = useState(false);
+    const [book, setBook] = useState({});
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,17 +18,56 @@ const CategoriesPage = ({ category }) => {
     }, []);
 
     return (
-        <div className="grid">
-            {
-                books.map(book => (
-                    <div key={book.id}>
-                        <img src={book.image} alt={book.title + "_image"} />
-                        <h1>{book.title}</h1>
-                        <h2>{book.publicationDate}</h2>
-                    </div>
-                ))
+        <>
+
+            <div className="grid">
+                {
+                    books.map(book => (
+                        <div className="book" key={book.id} onClick={() => {
+                            setBook(book);
+                            setShow(true);
+                        }}>
+                            <div className='cover'>
+                                <img src={book.cover} alt={book.title + "_image"} />
+                            </div>
+                            <div className="book-info">
+                                <h1>{book.title}</h1>
+                                <h2>{book.publicationYear}</h2>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            {show ?
+                <Modal show={show} onHide={() => setShow(false)} size="lg">
+                    <Modal.Header className='book-title'>
+                        <Modal.Title>{book.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className='cover-modal'>
+                            <img src={book.cover} alt={book.title + "_image"} />
+                        </div>
+                        <div className="modal-book-info">
+                            <h2>{book.sumup}</h2>
+                            <h3>{book.publicationYear}</h3>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShow(false)}>
+                            Fermer
+                        </Button>
+                        <Button variant="primary" onClick={() => setShow(false)}>
+                            Louer
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                :
+                null
+
             }
-        </div>
+
+
+        </>
     );
 };
 
