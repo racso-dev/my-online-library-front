3e6ef0c0a74e53903569b263c47af0ece8ed1f40
Oscar Renier <orenier@steamulo.com>
2022-09-20 11:00:27 +0200

    [UPDATE]: Navbar updated

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,13 +6,13 @@ import {
 
 import './AppBar.css';
 import { Pages } from '../routes/AppRouter';
-import * as AuthService from '../services/AuthService';
+import ProfileLogo from '../assets/profile.png';
 
 const AppBar = () => {
     const checkLoggedIn = () => {
         const token = localStorage.getItem('jwt');
         return token ? true : false;
-    }
+    };
     const [isLogged, setIsLogged] = React.useState(false);
 
     React.useEffect(() => {
@@ -22,24 +22,23 @@ const AppBar = () => {
     return (
         <Navbar className='app-navbar'>
             <Navbar.Brand href="/">
-                <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png" alt="logo" style={{ maxHeight: "3rem" }} />
+                <img src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png" alt="logo" />
                 <h1> MyBouquins </h1>
             </Navbar.Brand>
             <Nav className="nav">
                 <Nav.Link className="hover-lined" href={Pages.MAIN}>Accueil</Nav.Link>
-                <Nav.Link className="hover-lined" href={Pages.CATEGORIES}>Catégories</Nav.Link>
                 <Nav.Link className="hover-lined" href={Pages.OUR_BOOKS}>Nos Livres</Nav.Link>
                 <Nav.Link className="hover-lined" href={Pages.HOURS}>Horaires</Nav.Link>
                 <Nav.Link className="hover-lined" href={Pages.RULES}>Règlement</Nav.Link>
                 {
                     isLogged ?
-                        <Nav.Link className="hover-lined" href={Pages.MAIN} onClick={AuthService.signOut}>Déconnexion</Nav.Link>
+                        <Nav.Link className='logo' href={Pages.PROFILE}><img className='logo' src={ProfileLogo} alt="profile_logo"></img></Nav.Link>
                         :
                         <Nav.Link className="hover-lined" href={Pages.CONNECTION}>Connexion</Nav.Link>
                 }
             </Nav>
         </Navbar>
     );
-}
+};
 
 export default AppBar;
